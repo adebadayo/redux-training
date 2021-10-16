562bb84f43efc52237a45243b5e34bd4c208d517
adebadayo <adebayanen@gmail.com>
2021-10-17 01:03:34 +0900

    material-ui

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -1,19 +1,34 @@
 import React from "react";
+import {AppBar, List, ListItem, ListItemText, Reboot, Toolbar, Typography} from "material-ui";
+import {Button} from "material-ui";
+import Input from "material-ui/TextField/TextField";
 
 export default function TodoApp({task, tasks, inputTask, addTask}){
   return (
     <div>
-      <input type="text" onChange={(e) => inputTask(e.target.value)}/>
-      <input type="button" value="add" onClick={() => addTask(task)}/>
-      <ul>
-        {
-          tasks.map(function(item, i){
-            return (
-              <li key={i}>{item}</li>
-            );
-          })
-        }
-      </ul>
+      <Reboot />
+      <AppBar position="static">
+        <Toolbar>
+          <Typography type="title" color="inherit">
+            TODO
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <div style={ {padding : 16 }}>
+        <Input type="text" onChange={(e) => inputTask(e.target.value)}/>
+        <Button raised color="accent" onClick={() => addTask(task)}>add</Button>
+        <List>
+          {
+            tasks.map(function(item, i){
+              return (
+                <ListItem key={i}>
+                  <ListItemText primary={`・${item}`} />
+                </ListItem>
+              );
+            })
+          }
+        </List>
+      </div>
 
     </div>
   )
--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import { createStore } from "redux";
 import { render } from "react-dom";
 import { tasksReducer } from "./reducers/tasks";
 import TodoApp from "./containers/TodoApp";
+import {BrowserRouter as Router, Route} from "react-router-dom";
 
 const store = createStore(tasksReducer);
 
 render(
   <Provider store={store}>
-    <TodoApp />
+    <Router>
+      <Route path="/" component={TodoApp}></Route>
+    </Router>
   </Provider>,
   document.getElementById('root')
 );
